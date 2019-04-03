453e5dc54eefab425b6f59bd7110e6c1e2067d55
Dima Kagan <kagandi@bgu.ac.il>
2019-04-03 10:36:15 +0300

    Add SJR urls

--- ScienceDynamics/datasets/configs.py
+++ ScienceDynamics/datasets/configs.py
@@ -1,6 +1,5 @@
 MAG_URL = "https://www.dropbox.com/s/md69223d36htiep/MicrosoftAcademicGraph.zip"
-AMINER_URLS = ["https://academicgraphv1wu.blob.core.windows.net/aminer/aminer_papers_0.zip",
+AMINER_URLS = ("https://academicgraphv1wu.blob.core.windows.net/aminer/aminer_papers_0.zip",
                "https://academicgraphv1wu.blob.core.windows.net/aminer/aminer_papers_1.zip",
-               "https://academicgraphv1wu.blob.core.windows.net/aminer/aminer_papers_2.zip"]
-# "https://www.scimagojr.com/journalrank.php?year=2016&out=xls"
-# list(range(1999, 2018))
+               "https://academicgraphv1wu.blob.core.windows.net/aminer/aminer_papers_2.zip")
+SJR_URLS = (year, f"https://www.scimagojr.com/journalrank.php?year={year}&out=xls" for year in range(1999, 2018))
