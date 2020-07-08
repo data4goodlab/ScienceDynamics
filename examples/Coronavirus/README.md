520d31fcc85094e9e1ea43023249c412f1c7223d
Dim Kagan <dimakagan15@gmail.com>
2020-07-08 12:38:55 +0300

    Update readme and converted pubmed_2_json to cli tool.

--- examples/Coronavirus/README.md
+++ examples/Coronavirus/README.md
@@ -1,8 +1,10 @@
 # Coronavirus Research Analysis
 
 This folder contains the Jupyter notebooks used to calculate the results of Scientometric Trends for Coronaviruses and Other Emerging Viral Infections. To generate a new pubmed.json file (Pubmed only used in Journals Trends.ipynb ) pleases follow the following steps:
-1. Download pubmed xmls for Medline.
-2. pip install git+git://github.com/titipata/pubmed_parser.git
-3. Run pubmed_2_json.py
+1. [Download PubMed annual basline XMLs](https://www.nlm.nih.gov/databases/download/pubmed_medline.html). For example use wget to download the files: `wget -r ftp://ftp.ncbi.nlm.nih.gov/pubmed/baseline/*`
+2. `pip install git+git://github.com/titipata/pubmed_parser.git`
+3. Run `python pubmed_2_json.py  -i [path to the the directory containing pubmed data]`
 4. Put the generated json under Data/pubmed.
 5. Run the Pubmed section in Data Preprocessing.ipynb.
+
+
--- examples/Coronavirus/pubmed_2_json.py
+++ examples/Coronavirus/pubmed_2_json.py
@@ -2,10 +2,26 @@ import pubmed_parser as pp
 from pathlib import Path
 import json
 from tqdm import tqdm 
+import sys
+import argparse
+from itertools import islice
 
-res = []
-for xml_path in tqdm(Path("ftp.ncbi.nlm.nih.gov/pubmed/baseline").glob("*.xml.gz")):
-    res+= pp.parse_medline_xml(str(xml_path))
+if __name__ == "__main__":
 
-with open("pubmed.json", "w") as f:
-    f.write(json.dumps(res))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', required=True,
+                        help='Path to the pubmed data folder')
+    parser.add_argument('-l', type=int, default=0,
+                        help='Limit the number of files to read')
+    
+    args = vars(parser.parse_args())
+    res = []
+    pubmed_files = Path(args["i"]).rglob("*.xml.gz")
+
+    if args["l"]:
+        pubmed_files = islice(pubmed_files,0,args["l"])
+    for xml_path in tqdm(pubmed_files):
+        res+= pp.parse_medline_xml(str(xml_path))
+
+    with open("pubmed.json", "w") as f:
+        f.write(json.dumps(res))
