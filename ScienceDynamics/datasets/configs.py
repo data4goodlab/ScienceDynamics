6141a927d419d6d270c39f81fb782b479b5105eb
Dim Kagan <dimakagan15@gmail.com>
2020-07-14 10:35:26 +0300

    Aminer urls update

--- ScienceDynamics/datasets/configs.py
+++ ScienceDynamics/datasets/configs.py
@@ -16,9 +16,10 @@ MAG_URL_DICT = {"Affiliations":"https://zenodo.org/record/2628216/files/Affiliat
                "PaperUrls":"https://zenodo.org/record/2628216/files/PaperUrls.txt.gz?download=1"}
 
 NAME_GENDER_URL = "https://www.dropbox.com/s/hkrljwkj02hzgsh/first_names_gender.sframe.zip?dl=1"
-AMINER_URLS = ("https://academicgraphv1wu.blob.core.windows.net/aminer/aminer_papers_0.zip",
-               "https://academicgraphv1wu.blob.core.windows.net/aminer/aminer_papers_1.zip",
-               "https://academicgraphv1wu.blob.core.windows.net/aminer/aminer_papers_2.zip")
+AMINER_URLS = ("https://academicgraphv2.blob.core.windows.net/oag/aminer/paper/aminer_papers_0.zip",
+               "https://academicgraphv2.blob.core.windows.net/oag/aminer/paper/aminer_papers_1.zip",
+               "https://academicgraphv2.blob.core.windows.net/oag/aminer/paper/aminer_papers_2.zip",
+               "https://academicgraphv2.blob.core.windows.net/oag/aminer/paper/aminer_papers_3.zip")
 SJR_URLS = ((year, f"https://www.scimagojr.com/journalrank.php?year={year}&out=xls") for year in range(1999, 2019))
 SJR_OPEN_URLS = ((year, f"https://www.scimagojr.com/journalrank.php?openaccess=true&year={year}&out=xls") for year in range(1999, 2019))
 FIRST_NAMES_SFRAME = SFRAMES_BASE_DIR.joinpath('first_names_gender.sframe')
